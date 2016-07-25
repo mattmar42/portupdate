95003f5fb2b6d833bb0bbe06ceff15311d36c17e
Matt <matthewmaring42@gmail.com>
2016-07-25 17:53:17 -0400

    animation added to down arrow

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
   $('nav').fadeIn(2000);
   $('h1').delay(500).fadeIn(2000);
   $('h2').delay(900).fadeIn(2000);
-  $('p').delay(2000).fadeIn(2000);
-  $('h3').delay(3500).fadeIn(2000);
-  $('#down-arrow').delay(3750).fadeIn(2000);
+  $('p').delay(1500).fadeIn(2000);
+  $('h3').delay(2500).fadeIn(2000);
+  $('#down-arrow').delay(3000).fadeIn(2000);
 
 
 
