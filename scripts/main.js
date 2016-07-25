eadc651f559bdb548b1a4bbaffabab24187d9576
Matthew <matthewmaring42@gmail.com>
2016-07-25 02:18:19 -0400

    more content added, animations improved

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
 
   $('nav').fadeIn(2000);
   $('h1').delay(500).fadeIn(2000);
-  $('h2').delay(1000).fadeIn(2000);
-  $('p').delay(1500).fadeIn(2000);
+  $('h2').delay(900).fadeIn(2000);
+  $('p').delay(2000).fadeIn(2000);
+  $('h3').delay(3500).fadeIn(2000);
+  $('#down-arrow').delay(3750).fadeIn(2000);
+
 
 
 });
